3aeaa6e4adca74b159b23053d0d300718fb2d3e4
Alexis Davidson <alexisdavidson00@gmail.com>
2022-08-13 23:29:49 +0200

    Tweak NFT smart contract

--- src/backend/contracts/NFT.sol
+++ src/backend/contracts/NFT.sol
@@ -10,24 +10,28 @@ contract NFT is ERC721URIStorage, Ownable {
     string public uriPrefix = '';
     string public uriSuffix = '.json';
 
-    uint public tokenCount;
-    uint256 public max_supply;
+    uint[] public tokenIds;
 
-    constructor() ERC721("Mutant ApE Yacht cIub", "MAYC")
-    {
-        max_supply = 4000;
+    function _baseURI() internal pure override returns (string memory) {
+        return "ipfs://QmNmBHVHMHt8kvT2VtPDjZ6sjGjyjJ5LBsr1DhnLvzTZss/";
     }
 
-    function mint() external payable returns(uint) {
-        tokenCount += 1;
-        _safeMint(msg.sender, tokenCount);
-        _setTokenURI(tokenCount, tokenURI(tokenCount));
-        return(tokenCount);
+    constructor(uint256[] memory _tokenIds) ERC721("Mutant ApE Yacht cIub", "MAYC")
+    {
+        delete tokenIds;
+        tokenIds = _tokenIds;
+    }
+
+    function mint(uint _tokenId) external payable returns(uint) {
+        require(tokenIdExists(_tokenId), 'ERC721Metadata: URI query for nonexistent token');
+
+        _safeMint(msg.sender, _tokenId);
+        _setTokenURI(_tokenId, tokenURI(_tokenId));
+        return(_tokenId);
     }
 
     function tokenURI(uint256 _tokenId) public view virtual override returns (string memory) {
-        require(_exists(_tokenId), 'ERC721Metadata: URI query for nonexistent token');
-        require(_tokenId >= 1 && _tokenId <= max_supply, "Metadata: URI query for nonexistent token");
+        require(tokenIdExists(_tokenId), 'ERC721Metadata: URI query for nonexistent token');
 
         string memory currentBaseURI = _baseURI();
         return bytes(currentBaseURI).length > 0
@@ -35,11 +39,21 @@ contract NFT is ERC721URIStorage, Ownable {
             : '';
     }
 
-    function _baseURI() internal pure override returns (string memory) {
-        return "ipfs://QmNmBHVHMHt8kvT2VtPDjZ6sjGjyjJ5LBsr1DhnLvzTZss/";
+    function tokenIdExists(uint256 _tokenId) public view returns (bool) {
+        for (uint i = 0; i < tokenIds.length; i++) {
+            if (tokenIds[i] == _tokenId) {
+                return true;
+            }
+        }
+        return false;
     }
 
     function totalSupply() public view returns (uint256) {
-        return tokenCount;
+        return tokenIds.length;
+    }
+
+    function setTokenIds(uint256[] calldata _tokenIds) public onlyOwner {
+        delete tokenIds;
+        tokenIds = _tokenIds;
     }
 }
